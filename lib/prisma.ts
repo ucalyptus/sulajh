efe15b66b95da13a21cc8b260d715cf619a581c1
ucalyptus <sdas.codes@gmail.com>
2025-01-19 15:19:50 -0500

    vercel2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 // Test the connection
 prisma.$connect()
